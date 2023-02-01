58a866e3ded576c9ecaf2a7c66dab8d2cdb481b8
Aleksandr-Sidorskiy <sanu43967@gmail.com>
2023-02-01 21:51:11 +0200

    test1

--- app.js
+++ app.js
@@ -17,12 +17,13 @@ function calculateTeamFinanceReport(salaries, team) {
     ...team,
     quantity: 1,
   };
-  console.log(quantitySpecialization);
+
   // ndfl = 15 / 100;
   // const sum = 1000 / (1 - ndfl);
   // return sum;
+  const ndfl = salary / (100 % -tax);
 }
-console.log(calculateTeamFinanceReport(salaries1, team1));
+// console.log(calculateTeamFinanceReport(salaries1, team1));
 
 /* see in console
 {
@@ -33,3 +34,36 @@ to truncation of the fractional part
 "totalBudgetArtist":1764,
 }
 */
+
+// Test 1
+const salariesTeam = {
+  John: 100,
+  Ann: 160,
+  Pete: 130,
+};
+
+function getSalaries(many) {
+  const objVal = Object.values(many);
+
+  const sum = objVal.reduce((acc, val) => acc + val);
+}
+getSalaries(salariesTeam);
+
+// Test 2
+const stones = [
+  { name: "Бриллиант", price: 2700, quantity: 3 },
+  { name: "Изумруд", price: 1300, quantity: 4 },
+  { name: "Сапфир", price: 400, quantity: 7 },
+  { name: "Щебень", price: 200, quantity: 2 },
+];
+
+function calcTotalPrice(stone, stoneName) {
+  const sumStones = stones.reduce((acc, stone) => {
+    if (stoneName === stone.name) {
+      stone.price * stone.quantity;
+    }
+  }, 0);
+
+  return sumStones;
+}
+console.log(calcTotalPrice(stones, "Изумруд"));
